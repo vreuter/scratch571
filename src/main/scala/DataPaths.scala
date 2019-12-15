b79b7c65222c5c61be416032fede243e9e626bf3
Vince <vince.reuter@gmail.com>
2019-12-15 07:07:30 -0500

    file finding function

--- src/main/scala/DataPaths.scala
+++ src/main/scala/DataPaths.scala
@@ -1,7 +1,26 @@
 package flybrain571
 
+/**
+ * Functionality related to paths on disk
+ *
+ * @author Vince Reuter
+ */
 object DataPaths {
+
   import java.io.File
-  // TODO: *.sort.bam
-  // TODO: GTF for dm6 + possibly annotations
+  import scala.sys.process._
+
+  /**
+   * Find all files with a given extension and rooted in a given location.
+   *
+   * @param ext Extension required for a file to match
+   * @param root The folder from which to start the recursive search
+   * @return A collection of paths matching the extension, and within the filesystem 
+   *         subtree rooted as required
+   */
+  def findAllByExtension(ext: String)(root: File): Stream[String] = {
+    require(root.isDirectory, s"Root folder for find operation is not a directory: $root")
+    Seq("find", root.getPath, "-name", s"*${ext}").lineStream
+  }
+
 }
