0c891c3368780a4b25733eee5e5815998962af39
Vince <vince.reuter@gmail.com>
2019-12-15 07:08:12 -0500

    updating context specific values

--- src/main/scala/Preamble.scala
+++ src/main/scala/Preamble.scala
@@ -1,12 +1,24 @@
 package flybrain571
 
+import com.typesafe.scalalogging.LazyLogging
+
 /**
  * Project-specific data and such
  *
  * @author Vince Reuter
  */
-object Preamble {
+object Preamble extends LazyLogging {
+  import java.io.File
+  import MeTPeakProgram.{ ProgramInstance => MPP }
+  import Refinement._
   val neblAlias = "NEB_Antibody"
   val synSysAlias = "SynapticSystems_Antibody"
-  
+  // TODO: *.sort.bam
+  // TODO: GTF for dm6 + possibly annotations
+  val readsFileExt = ".sort.bam"
+  //private[this] val rawProgPath = new File(getClass().getResource("/runMetpeak.R").toString)
+  //logger.info(s"Program path: $rawProgPath")
+  //private[this] val progPath = ExtantFile.unsafe(rawProgPath)
+  //val mpProg = MPP(progPath)
+  val mpProg = MPP()
 }
