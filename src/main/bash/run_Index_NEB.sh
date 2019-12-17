c04e97d1df07287b39950caf591ea5c78cbe966a
Vince <vince.reuter@gmail.com>
2019-12-17 09:57:10 -0500

    add index script for NEB ab files

---
+++ src/main/bash/run_Index_NEB.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep3_Input_Mcherry_control.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep3_Input_Mcherry_HS.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep1_Input_Mcherry_HS.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep3_m6AIP_Ime4_HS.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep3_m6AIP_Ime4_control.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep2_m6AIP_Ime4_control.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep1_m6AIP_Mcherry_control.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep3_m6AIP_Mcherry_control.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep1_Input_Ime4_HS.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep1_m6AIP_Ime4_control.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep3_Input_Ime4_HS.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep2_Input_Ime4_control.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep2_m6AIP_Ime4_HS.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep2_Input_Mcherry_control.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep2_m6AIP_Mcherry_control.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep2_Input_Mcherry_HS.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep2_m6AIP_Mcherry_HS.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep1_Input_Mcherry_control.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep2_Input_Ime4_HS.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep1_m6AIP_Mcherry_HS.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep1_m6AIP_Ime4_HS.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep3_m6AIP_Mcherry_HS.sort.dedup.bam
+
+samtools index /home/vr/data/BoniniLab/NEB_Antibody/Rep3_Input_Ime4_control.sort.dedup.bam
+
