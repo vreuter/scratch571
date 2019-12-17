6125aa67ced1295b03aa5e5ac1db8d6af0408561
Vince <vince.reuter@gmail.com>
2019-12-17 16:25:16 -0500

    BED to sequence regions; rename

--- src/main/scala/Regions.scala
+++ src/main/scala/Regions.scala
@@ -1,13 +1,29 @@
 package flybrain571
 
-object Annotate {
+import com.typesafe.scalalogging.StrictLogging
+
+object Regions extends StrictLogging {
   
   import java.io.File
   import scala.io.Source
   import mouse.boolean._
   import FastaTools._, Refinement.{ ExtantFile }
   
-  def readAnnotationsFile(f: ExtantFile, geneIdIndex: Int = 0, sep: String = "\t"): Vector[(String, Range)] = {
+  /**
+   * Parse pairs of geneID and coordinates from BED.
+   *
+   * @param bed The BED to parse
+   * @return sequence of pairs of geneID ({@code name} field of BED per {@code MeTPeak}) and coordinates (0-based, incl/excl)
+   */
+  def regionsFromBed = (bed: ExtantFile) => Source.fromFile(bed.value).getLines.foldLeft(Vector.empty[(String, Range)]){
+    case (recs, line) => {
+      val fields = line.split("\t")
+      val newRec = fields(3) -> (fields(1).toInt -> fields(2).toInt)
+      recs :+ newRec
+    }
+  }
+
+  def readRegionsFile(f: ExtantFile, geneIdIndex: Int = 0, sep: String = "\t"): Vector[(String, Range)] = {
     Source.fromFile(f.value).getLines.foldLeft(Vector.empty[(String, Range)]){ case (recs, line) => {
       val fields = line.split(sep)
       if (fields.size < 3) throw new Exception(s"Too few fields (${fields.size}) from line: $line")
@@ -25,10 +41,11 @@ object Annotate {
     } }
   }
 
-  def sequenceRegions(fasta: ExtantFile)(regionsFile: ExtantFile): Either[String, Vector[(String, Range, String)]] = {
+  def sequenceRegions(fasta: ExtantFile)(bed: ExtantFile): Either[String, Vector[(String, Range, String)]] = {
+    logger.info(s"Associating regions from ${bed.value} using sequences from ${fasta.value} ")
     val seqMap = fbFasta2TranscriptExonSeqMap(fasta)
     val getSeq = regionSeq(seqMap) _
-    val (errors, result) = readAnnotationsFile(regionsFile).foldLeft(
+    val (errors, result) = regionsFromBed(bed).foldLeft(
       Vector.empty[String] -> Vector.empty[(String, Range, String)] ){ 
         case ((bads, goods), (id, range)) => 
           getSeq(id, range).fold(
