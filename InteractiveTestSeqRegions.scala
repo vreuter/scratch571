2861f6b6c73c1b7189ad015a36e173c42b0057f6
Vince <vince.reuter@gmail.com>
2019-12-17 18:57:52 -0500

    peak call BED to queries working

--- InteractiveTestSeqRegions.scala
+++ InteractiveTestSeqRegions.scala
@@ -3,6 +3,7 @@ package flybrain571
 object InteractiveTestSeqRegions {
   
   /* Imports and setup */
+  import scala.io.Source
   import java.io.{ BufferedWriter, File, FileWriter }
   import java.nio.file.Paths
   interp.load.ivy("org.typelevel" %% "cats-core" % "2.0.0")
@@ -23,6 +24,15 @@ object InteractiveTestSeqRegions {
   val faRecs = FastaTools.parseFbFasta(exonsFasta)
 
   val recsByGeneID = FastaTools.faRecords2ExonSeqMap(faRecs)
+
+  val getRegSeq = (geneID: String, coords: (Int, Int)) => FastaTools.regionSeq(recsByGeneID)(geneID, coords)
+
+  val metpeakCalls2Queries = (bed: ExtantFile) => Source.fromFile(bed.value).getLines.toList.tail map { l => {
+    val fields = l.split("\t")
+    fields(3) -> (fields(1).dropWhile(c => !c.isDigit).toInt -> fields(2).takeWhile(_.isDigit).toInt)
+  } }
+
+  val peakCallQueries = metpeakCalls2Queries(regFile)
 
   //val oneRes = Regions.addSeq2Regions(exonsFasta)(NEL(regFile, Nil))
 
