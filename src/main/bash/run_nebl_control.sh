6d37a06fd4dc7ff7663d4b13cc1628db2f368378
Vince <vince.reuter@gmail.com>
2019-12-17 19:57:05 -0500

    update filepaths in metpeak bash scripts

---
+++ src/main/bash/run_nebl_control.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# NEBL antibody, Ime4 IP vs. Input, control
+/home/vr/code/scratch571/src/main/resources/runMetpeak.R --gtf /home/vr/genomes/dm6/Drosophila_melanogaster.BDGP6.22.98.chr.gtf --ips /home/vr/data/BoniniLab/NEB_Antibody/Rep3_m6AIP_Ime4_control.sort.rmdup.bam /home/vr/data/BoniniLab/NEB_Antibody/Rep2_m6AIP_Ime4_control.sort.rmdup.bam /home/vr/data/BoniniLab/NEB_Antibody/Rep1_m6AIP_Ime4_control.sort.rmdup.bam --controls /home/vr/data/BoniniLab/NEB_Antibody/Rep1_Input_Ime4_control.sort.rmdup.bam /home/vr/data/BoniniLab/NEB_Antibody/Rep3_Input_Ime4_control.sort.rmdup.bam /home/vr/data/BoniniLab/NEB_Antibody/Rep2_Input_Ime4_control.sort.rmdup.bam -N Ime4_vs_Input_HS -O /home/vr/processed/BoniniLab --overwrite
+
+# NEBL antibody, Mcherry IP vs. Input, control
+/home/vr/code/scratch571/src/main/resources/runMetpeak.R --gtf /home/vr/genomes/dm6/Drosophila_melanogaster.BDGP6.22.98.chr.gtf --ips /home/vr/data/BoniniLab/NEB_Antibody/Rep2_m6AIP_Mcherry_control.sort.rmdup.bam /home/vr/data/BoniniLab/NEB_Antibody/Rep1_m6AIP_Mcherry_control.sort.rmdup.bam /home/vr/data/BoniniLab/NEB_Antibody/Rep3_m6AIP_Mcherry_control.sort.rmdup.bam --controls /home/vr/data/BoniniLab/NEB_Antibody/Rep3_Input_Mcherry_control.sort.rmdup.bam /home/vr/data/BoniniLab/NEB_Antibody/Rep1_Input_Mcherry_control.sort.rmdup.bam /home/vr/data/BoniniLab/NEB_Antibody/Rep2_Input_Mcherry_control.sort.rmdup.bam -N Mcherry_vs_Input_HS -O /home/vr/processed/BoniniLab --overwrite
+
